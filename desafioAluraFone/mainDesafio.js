fbc36f2dc60f07c2cb74d4c9b981ecf932065b7d
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-05-08 16:43:55 -0300

    editando js

--- desafioAluraFone/mainDesafio.js
+++ desafioAluraFone/mainDesafio.js
@@ -1,5 +1,12 @@
 //Escreva o código em JavaScript capaz de selecionar o elemento HTML input do tipo telefone
-document.querySelector('input[type=tel]')
+const inputTel = document.querySelector('input[type=tel]')
 
 const listaNumerosFone = document.querySelectorAll('input[type=button]')
 console.log(listaNumerosFone)
+
+for (let contador = 0; contador < listaNumerosFone.length; contador ++) {
+    const numero = listaNumerosFone[contador];
+    numero.onclick = function() {
+        inputTel.value = inputTel.value + numero.valeu
+    }
+}
--- main.js
+++ main.js
@@ -1,28 +1,47 @@
 function tocaSom (seletorAudio) {
-    document.querySelector(seletorAudio).play();
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert ('Elemento não encontrado ou seletor inválido')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); 
 //listaDeTeclas[0].onclick = tocaSomPom **para acessar individualmente 
 
-for (let contador= 0; contador < listaDeTeclas.length; contador++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function() {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
     }
 
     tecla.onkeydown = function(event) { //quando a tecla estiver pressionada
         console.log(event.code)
         if (event.code === 'Space' || 'Enter') {
-            tecla.classList.add('ativa')
+            tecla.classList.add('ativa');
             }   
         }
     
 
     tecla.onkeyup = function() { //quando a tecla for soltada
-        tecla.classList.remove('ativa')
+        tecla.classList.remove('ativa');
     }
+
 }
+
+document.querySelectorAll('.tecla').forEach(function(tecla) {
+    tecla.addEventListener('click', function(event) {
+        const elementoClicado = event.target;
+
+        if (elementoClicado.classList.contains('ativa')) {
+            elementoClicado.classList.remove('ativa');
+        } else {
+            elementoClicado.classList.add('ativa');
+        }
+    });
+});
